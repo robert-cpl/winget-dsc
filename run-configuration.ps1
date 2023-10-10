9fc975508665f42eb1aa466f6ce2e46b7394445a
Robert C <contact@copilau.me>
2023-10-10 21:48:26 +0200

    Update script url

--- run-configuration.ps1
+++ run-configuration.ps1
@@ -1,7 +1,7 @@
 # Check for admin rights
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Output "WinGet needs Administrator rights to run. Restarting in Admin mode..."
-    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://raw.githubusercontent.com/RobertCopilau/winget-config/main/run-configuration.ps1 | iex"
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://raw.githubusercontent.com/robert-cpl/winget-dsc/main/run-configuration.ps1 | iex"
     break
 }
 
@@ -49,7 +49,7 @@ $configurationFileName = "configuration.dsc.yaml"
 $configurationFilePath = "$configurationFolderPath/$configurationFileName"
 
 # Modules
-$fileFolderPath = if ($isLocalDevelopment) {"./configurations"} else {"https://raw.githubusercontent.com/RobertCopilau/winget-config/main/configurations"}
+$fileFolderPath = if ($isLocalDevelopment) {"./configurations"} else {"https://raw.githubusercontent.com/robert-cpl/winget-dsc/main/configurations"}
 $header = iwr -useb "$fileFolderPath/modules/header.yaml"
 $headerContent = $header.Content
 $footer = iwr -useb "$fileFolderPath/modules/footer.yaml"
