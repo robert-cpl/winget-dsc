4dfcd0467188d19ba2fbe1e674a15322909bf723
Robert C <contact@copilau.me>
2023-11-03 12:15:10 +0100

    Created the basics winform components: top bar with close, minimize buttons

--- test.ps1
+++ test.ps1
@@ -1,6 +1,15 @@
 
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
+using assembly System.Windows.Forms
+using namespace System.Windows.Forms
+using namespace System.Drawing
+
+### Top bar button settings
+$topBarbuttonSize = [Size]::new(40, 42);
+
+### Colors
+$primaryColor = [Color]::FromArgb(255, 25, 25, 25)
+$secondaryColor = [Color]::FromArgb(255, 33, 33, 33)
+$accentColor = [Color]::FromArgb(255, 48, 48, 48)
 
 $code = @"
 [System.Runtime.InteropServices.DllImport("gdi32.dll")]
@@ -12,77 +21,171 @@ $Win32Helpers = Add-Type -MemberDefinition $code -Name "Win32Helpers" -PassThru
 $horizontalResolution = $(wmic PATH Win32_VideoController GET CurrentHorizontalResolution)[2].Trim()
 $verticalResolution = $(wmic PATH Win32_VideoController GET CurrentVerticalResolution)[2].Trim()
 $form = [Form] @{
-    ClientSize = [Point]::new($($horizontalResolution / 1.5),$($verticalResolution / 1.5));
+    ClientSize      = [Point]::new($($horizontalResolution / 1.5), $($verticalResolution / 1.5));
     FormBorderStyle = [FormBorderStyle]::None;
-    BackColor = [SystemColors]::ControlLight;
-    Dock = [DockStyle]::Fill;
-    Text = "Desired State Configuration";
-    StartPosition = "CenterScreen";
+    BackColor       = $primaryColor;
+    Dock            = [DockStyle]::Fill;
+    Text            = "Desired State Configuration";
+    StartPosition   = "CenterScreen";
 }
-
 # Create a rounded corners
 $form.add_Load({
-    $hrgn = $Win32Helpers::CreateRoundRectRgn(0,0,$form.Width, $form.Height, 15,15)
-    $form.Region = [Region]::FromHrgn($hrgn)
-})
+        $hrgn = $Win32Helpers::CreateRoundRectRgn(0, 0, $form.Width, $form.Height, 15, 15)
+        $form.Region = [Region]::FromHrgn($hrgn)
+    })
 
-$lblTitle = [Label] @{
-    Text = "Desired State Configuration";
-    Font = [Font]::new("Microsoft Sans Serif", 20, [FontStyle]::Bold);
-    ForeColor = [SystemColors]::ControlDarkDark;
-    AutoSize = $true;
-    Location = [Point]::new(10,10);
+# Top bar 
+$topBar = [Panel] @{
+    BackColor = $secondaryColor;
+    Dock      = [DockStyle]::Top;
+    Height    = 40;
 }
-$form.Controls.Add($lblTitle)
+# Add label
+$topBarUnderline = [Label] @{
+    BorderStyle = [BorderStyle]::None;
+    Height      = 1;
+    BackColor   = $accentColor;
+    Dock        = [DockStyle]::Bottom;
+}
+$topBar.Controls.Add($topBarUnderline);
 
-# Create a close button
+# Add drag functionality to the top bar
+$topBar.Add_MouseDown( { 
+        $global:dragging = $true
+        $global:mouseDragX = [System.Windows.Forms.Cursor]::Position.X - $form.Left
+        $global:mouseDragY = [System.Windows.Forms.Cursor]::Position.Y - $form.Top
+    })
+
+# Move the form while the mouse is depressed (i.e. $global:dragging -eq $true)
+$topBar.Add_MouseMove( { if ($global:dragging) {
+            $screen = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea
+            $currentX = [System.Windows.Forms.Cursor]::Position.X
+            $currentY = [System.Windows.Forms.Cursor]::Position.Y
+            [int]$newX = [Math]::Min($currentX - $global:mouseDragX, $screen.Right - $form.Width)
+            [int]$newY = [Math]::Min($currentY - $global:mouseDragY, $screen.Bottom - $form.Height)
+            $form.Location = New-Object System.Drawing.Point($newX, $newY)
+        } })
+$topBar.Add_MouseUp( { $global:dragging = $false })
+$form.Controls.Add($topBar)
+
+# Add a icon to the top bar
+$icon = [PictureBox] @{
+    Image    = [Image]::FromFile("H:\repos\winget-dsc\visual-studio-code.png");
+    SizeMode = [PictureBoxSizeMode]::StretchImage;
+    Size     = [Size]::new(20, 20);
+    Location = [Point]::new(10, 10);
+}
+$topBar.Controls.Add($icon)
+
+# Add a title to the top bar
+$title = [Label] @{
+    Text      = "Winget DSC";
+    ForeColor = [Color]::White;
+    Font      = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
+    Location  = [Point]::new(40, 12);
+}
+$topBar.Controls.Add($title)
+
+# Create a close button with no border
 $closeFormButton = [Button] @{
-        Text = "X";
-        Font = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
-        ForeColor = [SystemColors]::ControlDarkDark;
-        BackColor = [SystemColors]::ControlLight;
-        FlatStyle = [FlatStyle]::Flat;
-        Size = [Size]::new(30,30);
-        Location = [Point]::new($($form.Width - 40), 10);
-    };
+    Text      = "X";
+    Font      = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
+    ForeColor = [Color]::White;
+    BackColor = $secondaryColor;
+    FlatStyle = [FlatStyle]::Flat;
+    Size      = $topBarbuttonSize
+    Location  = [Point]::new($($form.Width - 40), 0);
+}
 $closeFormButton.add_Click({
-    $form.Close()
-})
-$form.Controls.Add($closeFormButton)
+        $form.Close()
+    })
+$closeFormButton.FlatAppearance.BorderSize = 0
+$closeFormButton.FlatAppearance.BorderColor = $secondaryColor
+$topBar.Controls.Add($closeFormButton)
+# Add hover effect
+$closeFormButton.add_MouseEnter({
+        $closeFormButton.BackColor = [Color]::Red
+    })
+$closeFormButton.add_MouseLeave({
+        $closeFormButton.BackColor = $secondaryColor
+    })
 
 # Create minimize button
 $minimizeFormButton = [Button] @{
-        Text = "-";
-        Font = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
-        ForeColor = [SystemColors]::ControlDarkDark;
-        BackColor = [SystemColors]::ControlLight;
-        FlatStyle = [FlatStyle]::Flat;
-        Size = [Size]::new(30,30);
-        Location = [Point]::new($($form.Width - 80), 10);
-    };
+    Text      = "_";
+    Font      = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
+    ForeColor = [Color]::White;
+    BackColor = $secondaryColor;
+    FlatStyle = [FlatStyle]::Flat;
+    Size      = $topBarbuttonSize
+    Location  = [Point]::new($($form.Width - 80), 0);
+};
 $minimizeFormButton.add_Click({
-    $form.WindowState = [FormWindowState]::Minimized
-})
-$form.Controls.Add($minimizeFormButton)
+        $form.WindowState = [FormWindowState]::Minimized
+    })
+$minimizeFormButton.FlatAppearance.BorderSize = 0
+$minimizeFormButton.FlatAppearance.BorderColor = $secondaryColor
+$topBar.Controls.Add($minimizeFormButton)
 
-# Create maximize button
-$maximizeFormButton = [Button] @{
-        Text = "+";
-        Font = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
-        ForeColor = [SystemColors]::ControlDarkDark;
-        BackColor = [SystemColors]::ControlLight;
-        FlatStyle = [FlatStyle]::Flat;
-        Size = [Size]::new(30,30);
-        Location = [Point]::new($($form.Width - 120), 10);
-    };
-$maximizeFormButton.add_Click({
-    if ($form.WindowState -eq [FormWindowState]::Normal) {
-        $form.WindowState = [FormWindowState]::Maximized
-    } else {
-        $form.WindowState = [FormWindowState]::Normal
-    }
-})
-$form.Controls.Add($maximizeFormButton)
+# Add a button that acts as a dropdown menu with two choices
+$dropdownMenu = [ContextMenuStrip]@{
+    BackColor = $accentColor;
+    ForeColor = [Color]::White;
+}
+$dropdownMenu.add_Paint({
+        $hrgn = $Win32Helpers::CreateRoundRectRgn(0, 0, $dropdownMenu.Width, $dropdownMenu.Height, 10, 10)
+        $dropdownMenu.Region = [Region]::FromHrgn($hrgn)
+    })
+
+# Add menu items to the ContextMenuStrip
+$menuItem1 = [ToolStripMenuItem]@{
+    Text      = "Developer"
+    ForeColor = [Color]::White;
+    BackColor = $accentColor;
+    Size      = [Size]::new(70, 30);
+}
+$menuItem1.add_Click({ Write-Host "You selected Option 1" })
+$dropdownMenu.Items.Add($menuItem1)
+
+$menuItem2 = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuItem2.Text = "Personal"
+$menuItem2.add_Click({ Write-Host "You selected Option 2" })
+$dropdownMenu.Items.Add($menuItem2)
+
+# Create a new button
+$profileOptionsDropdown = [Button] @{
+    Text      = "Options";
+    Font      = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
+    ForeColor = [Color]::White;
+    BackColor = $accentColor;
+    FlatStyle = [FlatStyle]::Flat;
+    Size      = [Size]::new(100, 30);
+    Location  = [Point]::new(10, 50);
+};
+
+# add round corners to the button
+$profileOptionsDropdown.add_Paint({
+        $hrgn = $Win32Helpers::CreateRoundRectRgn(0, 0, $profileOptionsDropdown.Width, $profileOptionsDropdown.Height, 10, 10)
+        $profileOptionsDropdown.Region = [Region]::FromHrgn($hrgn)
+    })
+
+$profileOptionsDropdown.FlatAppearance.BorderSize = 0
+$profileOptionsDropdown.FlatAppearance.BorderColor = $accentColor
+
+# Add a click event to the button that shows the dropdown menu
+$profileOptionsDropdown.add_Click({
+        $dropdownMenu.Show($profileOptionsDropdown, 0, $profileOptionsDropdown.Height)
+    })
+
+# Add the button to the form
+$form.Controls.Add($profileOptionsDropdown)
+
+
+
+
+
+
+
 
 
 
