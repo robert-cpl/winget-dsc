1c53894c8c33e4fc29046257794f5c0409f202a2
Robert C <contact@copilau.me>
2023-11-03 19:30:25 +0100

    Added a left pannel for the profiles + function to quickly create the profile buttons

--- test.ps1
+++ test.ps1
@@ -3,6 +3,9 @@ using assembly System.Windows.Forms
 using namespace System.Windows.Forms
 using namespace System.Drawing
 
+### Profile button names
+$buttonNames = @("Developer", "Personal", "Tricent", "Custom")
+
 ### Top bar button settings
 $topBarbuttonSize = [Size]::new(40, 42);
 
@@ -40,7 +43,7 @@ $topBar = [Panel] @{
     Dock      = [DockStyle]::Top;
     Height    = 40;
 }
-# Add label
+# Underline
 $topBarUnderline = [Label] @{
     BorderStyle = [BorderStyle]::None;
     Height      = 1;
@@ -48,6 +51,22 @@ $topBarUnderline = [Label] @{
     Dock        = [DockStyle]::Bottom;
 }
 $topBar.Controls.Add($topBarUnderline);
+
+# Bottom bar
+$bottomBar = [Panel] @{
+    BackColor = $secondaryColor;
+    Dock      = [DockStyle]::Bottom;
+    Height    = 40;
+}
+# Underline
+$bottomBarUnderline = [Label] @{
+    BorderStyle = [BorderStyle]::None;
+    Height      = 1;
+    BackColor   = $accentColor;
+    Dock        = [DockStyle]::Top;
+}
+$bottomBar.Controls.Add($bottomBarUnderline);
+$form.Controls.Add($bottomBar)
 
 # Add drag functionality to the top bar
 $topBar.Add_MouseDown( { 
@@ -127,67 +146,79 @@ $minimizeFormButton.FlatAppearance.BorderSize = 0
 $minimizeFormButton.FlatAppearance.BorderColor = $secondaryColor
 $topBar.Controls.Add($minimizeFormButton)
 
-# Add a button that acts as a dropdown menu with two choices
-$dropdownMenu = [ContextMenuStrip]@{
-    BackColor = $accentColor;
-    ForeColor = [Color]::White;
+# Function that takes a list of button names and creates a buttons
+## Settings
+$buttonRoundness = 3
+$buttonSize = [Size]::new(100, 30)
+function CreateProfileButtons($buttonNames) {
+    $location = [Point]::new(10, 10)
+    $size = $buttonSize
+    $buttons = @()
+    $buttonNames | ForEach-Object -Begin { $i = 0 } -Process {
+        $button = [Button] @{
+            Text      = $buttonNames[$i];
+            Font      = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
+            ForeColor = [Color]::White;
+            BackColor = $accentColor;
+            FlatStyle = [FlatStyle]::Flat;
+            Size      = $size;
+            Location  = $location;
+        };
+        $button.add_Paint(({
+                    $hrgn = $Win32Helpers::CreateRoundRectRgn(0, 0, $this.Width, $this.Height, $buttonRoundness, $buttonRoundness)
+                    $this.Region = [Region]::FromHrgn($hrgn)
+                }).GetNewClosure())
+        
+        # On click event that adds < symbol as a label to the right of the button and clears it from the other buttons
+        $button.add_Click(({
+                    $label = [Label] @{
+                        Text      = "<";
+                        ForeColor = [Color]::White;
+                        Font      = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
+                        Location  = [Point]::new($($button.Location.X + $button.Width + 5), $($button.Location.Y + 5));
+                    }
+                    $buttons | ForEach-Object {
+                        # Remove any labels that contain < symbol
+                        if ($_.Controls.Text -eq "<") {
+                            echo "Removing label"
+                            $_.Controls.RemoveAt(0)
+                        }
+                    }
+                    $profileButtonArea.Controls.Add($label)
+                }).GetNewClosure())
+
+        
+
+        $button.FlatAppearance.BorderSize = 0
+        $button.FlatAppearance.BorderColor = $accentColor
+        $buttons += $button
+        $location.y += ($size.Height + 10 )
+        $i++
+    }
+    $profileButtonArea.Controls.AddRange($buttons)
 }
-$dropdownMenu.add_Paint({
-        $hrgn = $Win32Helpers::CreateRoundRectRgn(0, 0, $dropdownMenu.Width, $dropdownMenu.Height, 10, 10)
-        $dropdownMenu.Region = [Region]::FromHrgn($hrgn)
-    })
 
-# Add menu items to the ContextMenuStrip
-$menuItem1 = [ToolStripMenuItem]@{
-    Text      = "Developer"
-    ForeColor = [Color]::White;
-    BackColor = $accentColor;
-    Size      = [Size]::new(70, 30);
+# Area that hold the profile buttons and lives underneath the top bar
+$profileButtonArea = [Panel] @{
+    BackColor = $primaryColor;
+    Dock      = [DockStyle]::None;
+    Location  = [Point]::new(0, 40);
+    Size      = [Size]::new(140, $($form.Height - 80));
 }
-$menuItem1.add_Click({ Write-Host "You selected Option 1" })
-$dropdownMenu.Items.Add($menuItem1)
+$form.Controls.Add($profileButtonArea)
 
-$menuItem2 = New-Object System.Windows.Forms.ToolStripMenuItem
-$menuItem2.Text = "Personal"
-$menuItem2.add_Click({ Write-Host "You selected Option 2" })
-$dropdownMenu.Items.Add($menuItem2)
+# Add 1 px right border to the profile button area
+$profileButtonAreaBorder = [Label] @{
+    BorderStyle = [BorderStyle]::None;
+    Width       = 1;
+    BackColor   = $accentColor;
+    Dock        = [DockStyle]::Right;
+}
 
-# Create a new button
-$profileOptionsDropdown = [Button] @{
-    Text      = "Options";
-    Font      = [Font]::new("Microsoft Sans Serif", 10, [FontStyle]::Bold);
-    ForeColor = [Color]::White;
-    BackColor = $accentColor;
-    FlatStyle = [FlatStyle]::Flat;
-    Size      = [Size]::new(100, 30);
-    Location  = [Point]::new(10, 50);
-};
+$profileButtonArea.Controls.Add($profileButtonAreaBorder)
 
-# add round corners to the button
-$profileOptionsDropdown.add_Paint({
-        $hrgn = $Win32Helpers::CreateRoundRectRgn(0, 0, $profileOptionsDropdown.Width, $profileOptionsDropdown.Height, 10, 10)
-        $profileOptionsDropdown.Region = [Region]::FromHrgn($hrgn)
-    })
-
-$profileOptionsDropdown.FlatAppearance.BorderSize = 0
-$profileOptionsDropdown.FlatAppearance.BorderColor = $accentColor
-
-# Add a click event to the button that shows the dropdown menu
-$profileOptionsDropdown.add_Click({
-        $dropdownMenu.Show($profileOptionsDropdown, 0, $profileOptionsDropdown.Height)
-    })
-
-# Add the button to the form
-$form.Controls.Add($profileOptionsDropdown)
-
-
-
-
-
-
-
-
-
+# Create the buttons
+CreateProfileButtons($buttonNames)
 
 
 $form.ShowDialog()
